101e00e886466cafc50b26c2e8da0761709d7ee1
Ash Williams <ashjw91@gmail.com>
2015-05-01 20:51:14 -0700

    Better tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,16 +14,6 @@ class User < ActiveRecord::Base
 	has_secure_password
 	validates :password, length: { minimum: 6 }
 
-	def User.digest(string)
-		cost = ActiveModel::SecurePassword.min_cost ? 	BCrypt::Engine::MIN_COST :
-															BCrypt::Engine.cost
-		BCrypt::Password.create(string, cost: cost)
-	end
-
-	def User.new_token
-		SecureRandom.urlsafe_base64
-	end	
-
 	def remember
 		self.remember_token = User.new_token
 		update_attribute(:remember_digest, User.digest(remember_token))
@@ -37,4 +27,17 @@ class User < ActiveRecord::Base
 		return false if remember_digest.nil?
 		BCrypt::Password.new(remember_digest).is_password?(remember_token)
 	end												
+
+	class << self
+		def digest(string)
+			cost = ActiveModel::SecurePassword.min_cost ? 	BCrypt::Engine::MIN_COST :
+																BCrypt::Engine.cost
+			BCrypt::Password.create(string, cost: cost)
+		end
+
+		def new_token
+			SecureRandom.urlsafe_base64
+		end	
+	end
+
 end
